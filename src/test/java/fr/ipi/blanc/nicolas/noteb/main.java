b5d1b20c1d59a99f50b4eb289c449f575d8817a7
nicolas745 <47307856+nicolas745@users.noreply.github.com>
2024-01-11 14:14:11 +0100

    terminer note A et C

---
+++ src/test/java/fr/ipi/blanc/nicolas/noteb/main.java
@@ -0,0 +1,9 @@
+package fr.ipi.blanc.nicolas.noteb;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/test/java/fr/ipi/blanc/nicolas/notec/machine.java
+++ src/test/java/fr/ipi/blanc/nicolas/notec/machine.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -17,6 +18,7 @@ public class machine {
 	private ColumnsHandler columns = new ColumnsHandler();
 	private Integer mygain = 0;
 	private Integer nbjeton;
+	private int[] rntable;
 	public machine() {
 		nbjeton = columns.getjeton();
 		System.out.println(enummachine.BIENVENU.getDescription());
@@ -50,23 +52,45 @@ public class machine {
 			}
 			chargematrice();
 			nbjeton -= nb;
+        	try {
+				gain(nb);
+			} catch (exceptionsmachine e) {
+				e.printStackTrace();
+			}
 	        if (System.console() == null) {
-	        	try {
-					gain(nb);
-				} catch (exceptionsmachine e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+	        	//noteC marche que run as de eclipse (c'est que j'ai testé)
 	        	System.out.println("--------------------");
 	        	graphique();
 	        }else {
-
+	        	//noteA marche que exectable jar
+	        	animation();
+	        	System.out.print("\033[4;1H");
+	        	graphique();
+	        	select();
 	        }
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+    public void animation() {
+    	for(int i=0;i<3;i++) {
+    		for(int i2=rntable[i];i2<columns.getsize(i)+rntable[i];i2++) {
+    			for(int i3=0;i3<3;i3++) {
+    				for(int i4=0;i4<3;i4++) {
+        				replaceCharAt(2+4*i,4+i4, columns.getculumnvalue(i, i2+i4+1).charAt(0));
+    				}
+    				try {
+						TimeUnit.MILLISECONDS.sleep(50);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+    			}
+    		}
+    	}
+    	System.out.print("\033[7;1H");
+    }
 	public static List<List<String>> inverserMatrice(List<List<String>> matrice) {
         int lignes = matrice.size();
         int colonnes = matrice.get(0).size();
@@ -87,7 +111,7 @@ public class machine {
     }
 	public void chargematrice() {
 		matrice33 = new ArrayList<List<String>>();
-		int[] rntable = new int[3];
+		rntable = new int[3];
 		for(int i=0;i<3;i++) {
 			//rntable [i] = 0;
 			rntable [i] = (int) Math.round(Math.random()*columns.getsize(i));
@@ -103,8 +127,8 @@ public class machine {
 	}
 	private void affiche() {
 		for (List<String> list : matrice33){
-			String ligne = StringUtils.join(list,"|");
-			System.out.println(ligne);
+			String ligne =" "+StringUtils.join(list," | ")+" ";
+			System.out.println(ligne.replaceAll(" 7 ", "*7*").replaceAll("\\s([A-Z])\\s", "($1)").replaceAll(" BAR ","BAR"));
 		}
 		
 	}
@@ -118,23 +142,29 @@ public class machine {
 			case 1:
 				if(checkligne(1)) {
 					String selectcase = casename(matrice33.get(0).get(0));
-					mygain =listgain.get(selectcase).intValue();
+					mygain +=listgain.get(selectcase).intValue();
 				}
 				break;
 			case 2:
 				if(checkligne(0)) {
 					String selectcase = casename(matrice33.get(0).get(0));
-					System.out.println(selectcase);
-					System.out.println(listgain);
-					mygain =listgain.get(selectcase).intValue();
+					mygain +=listgain.get(selectcase).intValue();
 				}
 				if(checkligne(1)) {
 					String selectcase = casename(matrice33.get(1).get(0));
-					mygain =listgain.get(selectcase).intValue();
+					mygain +=listgain.get(selectcase).intValue();
 				}
 				if(checkligne(0)) {
 					String selectcase = casename(matrice33.get(2).get(0));
-					mygain =listgain.get(selectcase).intValue();
+					mygain +=listgain.get(selectcase).intValue();
+				}
+				if(matrice33.get(0).get(0)==matrice33.get(1).get(1) && matrice33.get(1).get(1)==matrice33.get(2).get(2)) {
+					String selectcase = casename(matrice33.get(1).get(1));
+					mygain +=listgain.get(selectcase).intValue();
+				}
+				if(matrice33.get(2).get(0)==matrice33.get(1).get(1) && matrice33.get(1).get(1)==matrice33.get(0).get(2)) {
+					String selectcase = casename(matrice33.get(1).get(1));
+					mygain +=listgain.get(selectcase).intValue();
 				}
 				break;
 			case 3:
